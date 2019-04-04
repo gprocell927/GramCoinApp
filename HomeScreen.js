c1fd5cbeb43c5a663545a0a77a29363c1709baa2
Gabi Procell <gprocell927@gmail.com>
2019-04-04 00:05:28 -0600

    Fetches current prices of currencies

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,15 +1,87 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import axios from 'axios';
+import { ActivityIndicator, View, Text, TouchableOpacity } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+
 
 export default class HomeScreen extends React.Component {
-    render () {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currencies: null,
+            isLoading: true,
+        }
+    }
+
+    getCurrencies(){
+        return fetch('https://api.coincap.io/v2/assets?ids=monero,bitcoin,dogecoin,litecoin')
+            .then((res) => res.json())
+            .then((resJson) => {                
+                console.log({resJson})
+                this.setState({
+                    currencies: resJson.data,
+                    isLoading: false
+                }, () => console.log('CURRENCY SET IN STATE', this.state.currencies));
+            })
+        .catch ((error) => {
+            console.log({error});
+        })
+    }
+        // axios.get(`https://api.coincap.io/v2/assets?ids=monero,bitcoin,dogecoin,litecoin`)
+        // .then(res => {
+        //     const currencies = res.data;
+        //     this.setState({ currencies })
+        //     .catch(err => console.tron.log({err}))
+        // })
+    
+    renderCurrency = ({item}) => (
+            <View>
+                <Text>Currency: {item.name}</Text>
+                <Text>Current Price: {item.priceUsd}</Text>
+            </View>
+        )
+
+    render() {
+        // if(this.state.isLoading){
+
+        //     return (
+        //     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        //         <ActivityIndicator/>
+        //       </View>
+    
+        //     )
+        // }
         return (
-        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-            <Text>Home Screen</Text>
-            <TouchableOpacity onPress={() => this.props.navigation.navigate('PriceHistory')}>
-                    <Text> Price History </Text>
+            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+            {this.state.loading && (
+                    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+                        <ActivityIndicator />
+                    </View>
+            )}
+                {!this.state.loading && (
+                <React.Fragment>
+                <Text>Home Screen</Text>
+                <TouchableOpacity onPress={() => this.getCurrencies()}>
+                            <Text>Get currencies</Text>
                     </TouchableOpacity>
-          </View>
+                {this.state.currencies && (
+                    <FlatList
+                        data={this.state.currencies}
+                        renderItem={this.renderCurrency} 
+                        extraData={this.state}
+                        />
+                )}
+                {!this.state.currencies && (
+                            <View><Text>NO RESULTS</Text></View>
+                )}
+
+                <TouchableOpacity onPress={() => this.props.navigation.navigate('PriceHistory')}>
+                    <Text> Price History </Text>
+                </TouchableOpacity>
+
+                </React.Fragment>
+            )}
+            </View>
 
         )
     }
